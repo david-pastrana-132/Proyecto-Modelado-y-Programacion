1119cd46c847e2ffd71d98ed715993ca6006bb57
david-pastrana-132 <davidpastrana132@ciencias.unam.mx>
2025-11-29 00:40:40 -0600

    Add files via upload

--- Juego.java
+++ Juego.java
@@ -9,17 +9,35 @@ public class Juego {
     private String genero;
     private int añoLanzamiento;
     private double calificacion; // 1-10
+    private String desarrollador; // 
+    private String editores; // 
+    private int duracionHoras; // 
+    private double precio; // 
+    private String comentarios; // 
+    private int numJugadores; //
+    private String conectividad;
+    private String clasificacionEdades;//
     private String rutaImagen; // Para la gestión de imágenes
-    private String estado; // "Completado", "En Progreso", etc.
+    
 
-    public Juego(String titulo, String plataforma, String genero, int año, double calificacion, String rutaImagen) {
+
+    public Juego(String titulo, String plataforma, String genero, int año, double calificacion, String rutaImagen, 
+                 String desarrollador, String editores, int duracionHoras, double precio, String comentarios, 
+                 int numJugadores, String conectividad, String clasificacionEdades) {
         this.titulo = titulo;
         this.plataforma = plataforma;
         this.genero = genero;
         this.añoLanzamiento = año;
         this.calificacion = calificacion;
         this.rutaImagen = rutaImagen;
-        this.estado = "Biblioteca";
+        this.desarrollador = desarrollador;
+        this.editores = editores;
+        this.duracionHoras = duracionHoras;
+        this.precio = precio;
+        this.comentarios = comentarios;
+        this.numJugadores = numJugadores;
+        this.conectividad = conectividad;
+        this.clasificacionEdades = clasificacionEdades; 
     }
 
     // Getters
@@ -41,13 +59,36 @@ public class Juego {
     public int getañoLanzamiento() { 
         return añoLanzamiento; 
     }
+    public String getDesarrollador() { 
+        return desarrollador;
+    }
+    public String getEditores() { 
+        return editores; 
+    }
+    public int getDuracionHoras() { 
+        return duracionHoras; 
+    }
+    public double getPrecio() { 
+        return precio;     
+    }
+    public String getComentarios() { 
+        return comentarios; 
+    }
+    public int getNumJugadores() { 
+        return numJugadores; 
+    }
+    public String getConectividad() { 
+        return conectividad; 
+    }
+    public String getClasificacionEdades() { 
+        return clasificacionEdades; 
+    }   
      @Override
     public String toString() {
         // Se usará, por ejemplo, en JList para mostrar el título y calificación
         return titulo + " (" + calificacion + ")";
     }
 
-    // AGREGA ESTOS SETTERS AL FINAL DE LA CLASE JUEGO
     public void setTitulo(String titulo) { 
         this.titulo = titulo; 
     }
@@ -66,7 +107,28 @@ public class Juego {
     public void setañoLanzamiento(int añoLanzamiento) { 
         this.añoLanzamiento = añoLanzamiento; 
     } 
-    public void setEstado(String estado) { 
-        this.estado = estado; 
+    public void setDesarrollador(String desarrollador) { 
+        this.desarrollador = desarrollador; 
+    }
+    public void setEditores(String editores) { 
+        this.editores = editores;
+    }
+    public void setDuracionHoras(int duracionHoras) { 
+        this.duracionHoras = duracionHoras; 
+    }
+    public void setPrecio(double precio) { 
+        this.precio = precio;
+    }
+    public void setComentarios(String comentarios) { 
+        this.comentarios = comentarios; 
+    }
+    public void setNumJugadores(int numJugadores) { 
+        this.numJugadores = numJugadores;
+    }
+    public void setConectividad(String conectividad) { 
+        this.conectividad = conectividad; 
+    }
+    public void setClasificacionEdades(String clasificacionEdades) { 
+        this.clasificacionEdades = clasificacionEdades;
     }
 }

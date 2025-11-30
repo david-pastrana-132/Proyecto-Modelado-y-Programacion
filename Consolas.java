08da7c2d7bf04c214993877094e5d43758dfb853
david-pastrana-132 <davidpastrana132@ciencias.unam.mx>
2025-11-30 17:06:51 -0600

    Add files via upload

---
+++ Consolas.java
@@ -0,0 +1,21 @@
+public class Consolas {
+    public static final String[] LISTA = {
+        // --- Nintendo ---
+        "Nintendo Switch 2", "Nintendo Switch", "Wii U", "Wii", "GameCube", "Nintendo 64", "SNES", "NES",
+        "Game Boy", "Game Boy Color", "Game Boy Advance", "Nintendo DS", "Nintendo 3DS",
+        
+        // --- PlayStation ---
+        "PlayStation 5", "PlayStation 4", "PlayStation 3", "PlayStation 2", "PlayStation 1",
+        "PSP", "PS Vita",
+        
+        // --- Xbox ---
+        "Xbox Series X/S", "Xbox One", "Xbox 360", "Xbox Original",
+        
+        // --- Sega ---
+        "Sega Dreamcast", "Sega Saturn", "Sega Genesis/Mega Drive", "Sega Master System", "Game Gear",
+        
+        // --- Retro / Otros ---
+        "PC", "Atari 2600", "Atari 7800", "Neo Geo", "Commodore 64", "Amiga",
+        "Arcade", "Mobile (Android/iOS)", "Otro"
+    };
+}
